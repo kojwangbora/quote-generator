dec0bee6dc79ce32d79d3da7ad26949360ab0518
kojwangbora <kojwangbora254@gmail.com>
2022-02-06 17:58:20 +0300

    generate quote component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,21 @@
 export class Quotes {
+
+    public author:string
+    public name: string
+    public title:string
+    public quote: string
+    showInfo:boolean
+ constructor(author:string, name:string,title:string, quote:string,public datePosted:Date){
+    this.author=author
+    this.name=name
+    this.title= title
+    this.quote=quote;
+    this.showInfo=true
+ }
+     
+  
+    
+
+
+     
 }
