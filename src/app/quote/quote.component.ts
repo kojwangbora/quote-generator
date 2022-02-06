aa92ebce961b0aca286717a6cbd8099e3599c0b0
kojwangbora <kojwangbora254@gmail.com>
2022-02-06 18:48:37 +0300

    add bootstrap

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes:Quotes[]=[
+    new Quotes('Eli-k', 'George', 'life sayings','hard work pays', new Date(2000, 10, 10)),
+    new Quotes('patrick', 'Sarah', 'Business sayings','Buy one get one free', new Date(2020, 1, 1)),
+    new Quotes('paick', 'Sarh', 'Street-tech sayings','When the going gets tough, the tough get going', new Date(2011, 11, 11)),
+    new Quotes('aick', 'Sharh', 'Tech sayings','Do not tamper with the code as long it works', new Date(2012, 10, 10))
+  ]
+   
   constructor() { }
 
   ngOnInit(): void {
   }
+  toggleDetails(index){
+    this.quotes[index].showInfo=!this.quotes[index].showInfo
+  }
+  
 
 }
